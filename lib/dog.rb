7c215c5bdb4f4fb51289f1b0c2aa7816c65bf766
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-17 22:45:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,6 +39,12 @@ class Dog
   end
   
   def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+    
+    
     new_dog = self.new
     new_dog.id = row[0]
     new_dog.name = [1]
